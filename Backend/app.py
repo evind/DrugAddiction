6924cdc0137d06c4b0a0e9aa925cf8e46bffacda
Evin Darling <hi@evin.io>
2021-04-29 12:55:18 +0100

    Add basic API calls to retrieve data from database tables

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,45 @@
 from flask import Flask
-
+import requests
+from data_utils import *
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello, world'
+
+
+@app.route('/patients')
+def get_patients():
+    data = get_all_patients()
+    return data
+
+
+@app.route('/patients/<int:id>')
+def get_a_patient(id):
+    data = get_patient(id)
+    return data
+
+
+@app.route('/doctors')
+def get_doctors():
+    data = get_all_doctors()
+    return data
+
+
+@app.route('/doctors/<int:id>')
+def get_a_doctor(id):
+    data = get_doctor(id)
+    return data
+
+
+@app.route('/questionnaires')
+def get_questionnaires():
+    data = get_all_questionnaires()
+    return data
+
+
+@app.route('/questionnaires/<int:id>')
+def get_a_questionnaire(id):
+    data = get_questionnaire(id)
+    return data
---
+++ Backend/data_utils.py
@@ -0,0 +1,64 @@
+import mariadb
+from flask import jsonify
+
+
+try:
+    conn = mariadb.connect(
+        user="da_admin",
+        password="temporary",
+        host="127.0.0.1",
+        port=3306,
+        database="drug_addiction"
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to database: {e}")
+
+conn.autocommit = True
+
+
+def get_all_doctors():
+    SQL = "SELECT * FROM doctors"
+    with conn.cursor(dictionary=True) as cursor:
+        cursor.execute(SQL)
+        data = cursor.fetchall()
+    return jsonify(data)
+
+
+def get_doctor(id):
+    SQL = "SELECT * FROM doctors WHERE id=?"
+    with conn.cursor(dictionary=True) as cursor:
+        cursor.execute(SQL, (id,))
+        data = cursor.fetchall()
+    return jsonify(data)
+
+
+def get_all_patients():
+    SQL = "SELECT * FROM patients"
+    with conn.cursor(dictionary=True) as cursor:
+        cursor.execute(SQL)
+        data = cursor.fetchall()
+    return jsonify(data)
+
+
+def get_patient(id):
+    SQL = "SELECT * FROM patients WHERE id=?"
+    with conn.cursor(dictionary=True) as cursor:
+        cursor.execute(SQL, (id,))
+        data = cursor.fetchall()
+    return jsonify(data)
+
+
+def get_all_questionnaires():
+    SQL = "SELECT * FROM questionnaires"
+    with conn.cursor(dictionary=True) as cursor:
+        cursor.execute(SQL)
+        data = cursor.fetchall()
+    return jsonify(data)
+
+
+def get_questionnaire(id):
+    SQL = "SELECT * FROM questionnaires WHERE id=?"
+    with conn.cursor(dictionary=True) as cursor:
+        cursor.execute(SQL, (id,))
+        data = cursor.fetchall()
+    return jsonify(data)
